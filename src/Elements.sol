b4eec8a50de4cf35a26b40264bebc8ba753b993e
Colin Platt <colingplatt@gmail.com>
2023-07-21 18:21:08 +0200

    Track Register outline

---
+++ src/Elements.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Ownable} from "lib/solady/src/Milady.sol";
+
+library Elements {
+
+    
+
+
+
+}
--- src/Track_Register.sol
+++ src/Track_Register.sol
@@ -1,14 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Track_Register {
-    uint256 public number;
+import {Ownable} from "lib/solady/src/Milady.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Track_Register is Ownable {
+
+    struct SLOT {
+        uint8 element_0;
+        uint8 element_1;
+        uint8 element_2;
+        uint8 element_3;
     }
 
-    function increment() public {
-        number++;
+    struct TRACK {
+        SLOT[10] slot;
     }
+
+    struct ELEMENT {
+        string name;
+        function() internal pure returns(bytes memory) fn;
+
+    }
+
+    mapping (uint8 => ELEMENT) elements;
+
+    constructor() {
+        _initializeOwner(msg.sender);
+    }
+
+    function _initElementFunctions(
+        uint8 idx, 
+        function() internal pure returns(bytes memory) _fn, 
+        string memory _name
+    ) internal {
+        elements[idx] = ELEMENT(_name, _fn);
+    }
+
+    function playElement(uint8 idx) public view returns (bytes memory) {
+        return elements[idx].fn();
+    }
+
+    function elementName(uint8 idx) public view returns (string memory) {
+        return elements[idx].name;
+    }
+
 }
