37dbe83a884fb0c1e52ab0c1330f32d966c140f2
Colin Platt <colingplatt@gmail.com>
2023-07-23 04:07:53 +0200

    last updates

--- script/Track_Register.s.sol
+++ script/Track_Register.s.sol
@@ -33,6 +33,37 @@ contract TrackScript is Script {
     }
 }
 
+
+contract FillGridScript is Script {
+    Track_Register public registry = Track_Register(0x6F9bc2c85521f0FbDe8F8Bc40Aa5ed8Fdd3b9B92);
+    TrackURI public uri;
+
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // track, slot, channel, val
+
+
+            registry.writeToSlot(0, 0, 2, uint8(0));
+            registry.writeToSlot(0, 1, 2, uint8(0));
+            registry.writeToSlot(0, 2, 2, uint8(0));
+            registry.writeToSlot(0, 3, 2, uint8(0));
+            registry.writeToSlot(0, 4, 2, uint8(0));
+            registry.writeToSlot(0, 5, 2, uint8(0));
+            registry.writeToSlot(0, 6, 2, uint8(0));
+            registry.writeToSlot(0, 7, 2, uint8(0));
+            registry.writeToSlot(0, 8, 2, uint8(0));
+            registry.writeToSlot(0, 9, 2, uint8(0));
+
+        vm.stopBroadcast();
+    }
+}
+
+
 contract TrackURIScript is Script {
     Track_Register public registry = Track_Register(0x2Fb2ba909F2D2B55c4A70AEaE54cFFba2cE72205);
     TrackURI public uri;
